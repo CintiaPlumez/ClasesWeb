00ad68228754b75b7f7e88ac9ed9e5bdd8086f07
cintia <cintiaplumez@gmail.com>
2025-02-06 11:39:33 -0300

    cambios en clases particulares

---
+++ gloopbox.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gloop.Box</title>
+    <link rel="stylesheet" href="gloopstyle.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Gloop.Box</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+        <main>
+        
+          <img src="LOGOGLOOP.jpeg"" alt=".....">
+        <footer>
+            <?php
+                echo "@gloopbox  |  [Wp]: 2262 - 30 - 1234   |  Neochea BA Arg";          
+            ?>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ imc.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora IMC</title>
+    <link rel="stylesheet" href="StyleImc.css">
+</head>
+
+<body>
+    <h2>CCALCULADORA IMC</h2>
+    <h3>- Indice de Masa Corporal - </h3>
+    <h4>Ingrese los datos requeridos</h4>
+    <div>
+        <p> IMC= Peso (kg)/Talla2 (m2) </p>
+        <form action="imc.php" method="get">
+
+            <input type="text" name="nombre" placeholder="Nombre">
+            <br>
+            <input type="number" name="edad" placeholder="Edad">
+            <br>
+            <input type="number" name="peso" placeholder="Peso (en kilos)" step="any">
+            <br>
+            <input type="number" name="altura" placeholder="Altura (en metros)" step="any"> 
+            <br>        
+            <button type="submit">Calcular</button>
+        </form>
+    </div>
+    <div>
+        <h2> Resultado: </h2>
+        <?php 
+            $nombre=$_GET["nombre"];
+            $edad=$_GET["edad"];
+            $peso=$_GET["peso"];
+            $altura=$_GET["altura"];
+
+            $IMC = $peso/($altura*$altura);
+            $IMC= number_format ($IMC,2);
+
+
+            echo "<h3>".$IMC."</h3>";
+            
+            if ($IMC <18.5) 
+                {echo"<h3> bajo peso </h3>";}
+            elseif ($IMC >=18.5 && $IMC <25) 
+                {echo "<h3> peso normal </h3>";}
+            elseif ($IMC >=25 && $IMC <30) 
+                {echo "<h3> sobrepeso </h3>";}
+            else
+                {echo "<h3> obesidad </h3>";}
+ 
+            // hacer un if donde pueda imprimir el diagnóstico
+        ?>
+    </div>
+</body>
+</html>
